875809004ec45ca654d31e9319756ba488304b03
kagenza isaac <kagisaac103@gmail.com>
2023-12-10 12:06:50 +0200

    Nectaus fn web app inital commit

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/bootstrap-5.0.2-dist/css/bootstrap.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<nav id="main-navbar">
+    <div class="log"><img src="{{asset('assets/nectausProLog2.png')}}" alt="log"> </div>
+    <div class="nav-links">
+        <ul>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Services</li>
+            <li>Mobile App</li>
+            <li>News</li>
+            <li>Login</li>
+
+
+
+        </ul>
+    </div>
+
+</nav>
+
+<body>
+    <!-- ///////////////   hero      /////////////////// -->
+    <div class="hero-section .bg-oppacity">
+
+        <div class="backgroung-oppasity">
+            <div class="hero-section-content">
+                <h1>Get Premium Bees Habbitant condition</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laborum, libero eaque obcaecati
+                    accusamus
+                    aperiam voluptate blanditiis voluptatum accusantium voluptas earum quam officiis perspiciatis iure
+                    dolores
+                    ducimus iste ad sequi?</p>
+                <Span>
+                    <button>Learn More</button> <button>Learn More</button>
+                </Span>
+            </div>
+        </div>
+
+        <div class="backgroung-oppasity">
+            <div class="hero-section-content">
+                <h1>Pollinate Your Crops With Precision</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laborum, libero eaque obcaecati
+                    accusamus
+                    aperiam voluptate blanditiis voluptatum accusantium voluptas earum quam officiis perspiciatis iure
+                    dolores
+                    ducimus iste ad sequi?</p>
+                <Span>
+                    <button>Learn More</button> <button>Learn More</button>
+                </Span>
+            </div>
+        </div>
+    </div>
+    <div class="main-content">
+
+
+        <!-- ///////////////   profile cards      /////////////////// -->
+        <div class="profile-cards">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="/assets/cardBees.jpg" alt="Avatar" style="width:300px;height:300px;">
+                        <h2>beekeeping </h2>
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Modern beekeeping</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aliquid corrupti hic adipisci
+                            saepe error alias consectetur omnis, doloribus qui eligendi, voluptate voluptates ipsa
+                            repudiandae quaerat explicabo debitis quae minima?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="/assets/beesPollination2.avif" alt="Avatar" style="width:300px;height:300px;">
+                        <h2>Pollination service</h2>
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Bee Pollination</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aliquid corrupti hic adipisci
+                            saepe error alias consectetur omnis, doloribus qui eligendi, voluptate voluptates ipsa
+                            repudiandae quaerat explicabo debitis quae minima?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card-box">
+
+
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front " id="front">
+                            <img src="/assets/beeProduct.jpg" alt="Avatar" style="width:300px;height:300px;">
+                            <h2>Bee Product</h2>
+
+                        </div>
+                        <div class="flip-card-back">
+
+                            <h1>Bee Product</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aliquid corrupti hic
+                                adipisci
+                                saepe error alias consectetur omnis, doloribus qui eligendi, voluptate voluptates ipsa
+                                repudiandae quaerat explicabo debitis quae minima?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- ///////////////   About      /////////////////// -->
+        <div class="abautUs-section">
+            <div class="about-annimation">
+                <img src="/assets/beeProduct.jpg" alt="Avatar" style="width: 400px;height: 400px;">
+            </div>
+            <div class=" about-content">
+                <h1>ABOUT US</h1>
+                <h3>We Preserve & Mantain Bees For Pollination</h3>
+                <p>Curabitur blandit ipsum taciti penatibus mus pellentesque per consequat mi phasellus. Ultrices ad
+                    commodo augue inceptos sagittis sociosqu blandit cursus pretium. Dui mus netus a class et habitasse
+                    curae id vitae. Risus donec volutpat praesent amet convallis condimentum ultrices maecenas proin.
+                </p>
+                <button>Discover More</button>
+            </div>
+
+        </div>
+        <!-- ///////////////   work      /////////////////// -->
+
+
+        <div class="work-section">
+            <div class="work-content">
+
+                <h6>OUR SERVICES</h6>
+                <h1>We Raise and Keep Exceptional Bees</h1>
+                <p>By combining generations of commercial beekeeping experience with cutting-edge technology.</p>
+            </div>
+            <div class="work-cards">
+                <div class="workCard">
+                    <h3>Smart Hiving</h3>
+                    <img src="/assets/cardBees.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+                <div class="workCard">
+                    <h3>Pollinating</h3>
+                    <img src="/assets/beeBg.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+                <div class="workCard">
+                    <h3>Honey Production</h3>
+                    <img src="/assets/beeProduct.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+                <div class="workCard">
+                    <h3>Honey Production</h3>
+                    <img src="/assets/beeProduct.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+                <div class="workCard">
+                    <h3>Honey Production</h3>
+                    <img src="/assets/beeProduct.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+                <div class="workCard">
+                    <h3>Honey Production</h3>
+                    <img src="/assets/beeProduct.jpg" alt="" style="height: 200px; width: 300px;">
+                    <div class="workCard-text-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nobis in tenetur cum
+                            eligendi.
+                            Possimus deleniti excepturi sit eos, similique velit eum enim vitae corporis sequi
+                            labore
+                            impedit beatae voluptatibus!</p>
+                        <button>learn more</button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+        <!-- /////////// PRODUCT \\\\\\\\\\\\\\\\  -->
+        <div class="product-section">
+            <div class="advt-section">
+                <!-- <h3>We pledge for 100% satisfaction</h3> -->
+            </div>
+            <div class="product-content-cards">
+                <div class="product-content ">
+
+                    <h6>OUR SERVICES</h6>
+                    <h1>We Raise and Keep Exceptional Bees</h1>
+                    <p>By combining generations of commercial beekeeping experience with cutting-edge technology.</p>
+                </div>
+                <div class="product-cards">
+                    <div class="productCard">
+                        <img src="/assets/cardBees.jpg" alt="" style="width:250px; height: 150px;">
+                        <div class="productCard-content">
+
+                            <h3>Smart Hive</h3>
+                            <ul>
+                                <li>Real-Time Insight</li>
+                                <li>Actuation & Automation </li>
+                                <li>Future prediction</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="productCard">
+                        <img src="/assets/beeWax.jpg" alt="" style="width:250px; height: 150px;">
+                        <div class="productCard-content">
+
+                            <h3>Bee wax</h3>
+                            <ul>
+                                <li>Real-Time Insight</li>
+                                <li>Actuation & Automation </li>
+                                <li>Future prediction</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="productCard">
+                        <img src="/assets/royaljelly.jpg" alt="" style="width:250px; height: 150px;">
+                        <div class="productCard-content">
+
+                            <h3>Royal Jelly</h3>
+                            <ul>
+                                <li>Real-Time Insight</li>
+                                <li>Actuation & Automation </li>
+                                <li>Future prediction</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+        <!-- ///////////////   testimonial      /////////////////// -->
+
+        <!-- <div class=" testimonial-section">
+                        <div class="work-content">
+                            <h3>We Raise and Keep Exceptional Bees</h3>
+                            <p>by combining generations of commercial beekeeping experience with cutting-edge
+                                technology.</p>
+                        </div>
+                        <div class="work-annimation">
+                            <h1>image</h1>
+                        </div>
+                    </div> -->
+        <!-- ///////////////   paterner      /////////////////// -->
+
+        <div class="patener-section">
+            <div class="patener-content">
+                <h3>We Raise and Keep Exceptional Bees</h3>
+                <p>by combining generations of commercial beekeeping experience with cutting-edge
+                    technology.</p>
+            </div>
+            <div class="work-annimation">
+                <h1>image</h1>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+<footer>
+    <div class="footer-section">
+        <div class="log"><img src="{{asset('assets/nectausProLog2.png')}}" alt="log"> </div>
+        <div class="footer-adreess">
+
+            <h2>Address</h2>
+            4225 S Highland Ave. <br>
+            1-855-4-BEEHERO <br>
+            Del Rey, CA 93616 <br>
+            hello@beehero.io
+
+        </div>
+        <div class="footer-nav-links">
+            <h2>Quick Links</h2>
+            <ul>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>News</li>
+                <li>Mobiel app</li>
+                <li>Login ds</li>
+                <li>SignUp</li>
+            </ul>
+        </div>
+        <div class="footer-contact-form">
+            <form action="">
+                <h2>Contact Us</h2>
+                <label for="">Full Name</label>
+                <input type="text">
+                <label for="">Email</label>
+                <input type="email">
+                <label for="">Message</label>
+                <textarea name="" id="" cols="30" rows="3"></textarea>
+            </form>
+        </div>
+    </div>
+    <div class="sub-footer">
+        <p>© 2023 BeeHero. All rights reserved.</p>
+    </div>
+</footer>
+<script src="/bootstrap-5.0.2-dist">
+</script>
+
+</html>
